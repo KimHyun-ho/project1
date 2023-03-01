ea070ce2f2e9a81456465e251e3ea9d3ae287059
jyjy1007 <117172261+jyjy1007@users.noreply.github.com>
2023-03-01 17:25:05 +0900

    캘린더(의사) 디비 연동 및 조회 기능 추가

---
+++ AnimalHospitalServiceProject/src/main/java/kr/human/anihospital/service/ScheduleService.java
@@ -0,0 +1,11 @@
+package kr.human.anihospital.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ScheduleService {
+	//----------------------------------------------------------------------------------------------------
+	// 의사 스케줄을 풀캘린더에 표시해줄 메서드
+	//----------------------------------------------------------------------------------------------------
+	List<Map<String, Object>> selectScheduleDoctorArrJson(int seqDoctor) throws Exception;
+}
