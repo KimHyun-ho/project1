5f1eb553d01166e893780612b12325d6ee380b4f
jyjy1007 <117172261+jyjy1007@users.noreply.github.com>
2023-03-05 15:33:32 +0900

    환자 리스트 검색기능 (작성중)

--- AnimalHospitalServiceProject/src/main/java/kr/human/anihospital/controller/DoctorPageController.java
+++ AnimalHospitalServiceProject/src/main/java/kr/human/anihospital/controller/DoctorPageController.java
@@ -1,8 +1,6 @@
 package kr.human.anihospital.controller;
 
-import java.util.HashMap;
 import java.util.List;
-
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.human.anihospital.service.DoctorPageService;
+import kr.human.anihospital.vo.DiagnosisInfoVO;
+import kr.human.anihospital.vo.DocPatientInfoVO;
 import kr.human.anihospital.vo.DoctorInfoVO;
 import kr.human.anihospital.vo.MedicineInfoVO;
 import kr.human.anihospital.vo.PagingVO;
-import kr.human.anihospital.vo.DiagnosisInfoVO;
-import kr.human.anihospital.vo.DocPatientInfoVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -49,7 +47,8 @@ public class DoctorPageController {
 	// ### 수의사 전용 페이지 -> 나의 정보 조회 -> 수정하기 ####
 	// #########################################################
 	@PostMapping("/doctorInfoEdit")
-	public String doctorInfoEdit(@RequestParam Map<String, String> map, Model model) {
+	public String doctorInfoEdit(@RequestParam Map<String, String> map, 
+								 Model model) {
 		log.info("받은 map : {}", map);
 		model.addAttribute("map", map);
 		return "doctorInfoEdit";
@@ -59,7 +58,8 @@ public class DoctorPageController {
 	// ### 수의사 전용 페이지 -> 나의 정보 조회 -> 수정하기 -> 수정하기 ####
 	// #####################################################################
 	@PostMapping("/doctorInfoEditOk")
-	public String doctorInfoEditOk(@ModelAttribute DoctorInfoVO doctorInfoVO, MultipartFile file) {
+	public String doctorInfoEditOk(@ModelAttribute DoctorInfoVO doctorInfoVO, 
+								   MultipartFile file) {
 		log.info("doctorInfoVO 받은 값 : {} {}", doctorInfoVO, file);
 		doctorPageService.updateOneDoctorInfoVO(doctorInfoVO, file);
 		return "redirect:doctorInfo";
@@ -69,8 +69,10 @@ public class DoctorPageController {
 	// #### 수의사 전용 페이지 -> 진료내역 조회 ####
 	// #############################################
 	@GetMapping("/patientInfoList")
-	public String patientInfoList(@RequestParam(defaultValue = "1") int c, @RequestParam(defaultValue = "10") int p,
-			@RequestParam(defaultValue = "10") int b, Model model) {
+	public String patientInfoList(@RequestParam(defaultValue = "1") int c, 
+								  @RequestParam(defaultValue = "10") int p,
+								  @RequestParam(defaultValue = "10") int b, 
+								  Model model) {
 		// #########################################################
 		// ## seqDoctor는 로그인 정보 Session에서 받아와야 합니다.##
 		// #########################################################
@@ -87,7 +89,8 @@ public class DoctorPageController {
 	// #### 수의사 전용 페이지 -> 진료내역 조회 -> 환자 정보 조회 ####
 	// ###############################################################
 	@GetMapping("/patientInfo")
-	public String patientInfo(@RequestParam int seqAnimal, Model model) {
+	public String patientInfo(@RequestParam int seqAnimal, 
+							  Model model) {
 		DocPatientInfoVO patientInfoVO = doctorPageService.selectOnePatientInfoVO(seqAnimal);
 		List<DiagnosisInfoVO> diagnosisInfoVOs = doctorPageService.selectAllPatientDiaRecord(seqAnimal);
 		model.addAttribute("patientInfoVO", patientInfoVO);
@@ -101,7 +104,8 @@ public class DoctorPageController {
 	// #### 수의사 전용 페이지 -> 진료내역 조회 -> 환자 정보 조회 -> 환자정보 수정 ####
 	// ################################################################################
 	@PostMapping("/patientInfoEditDoctor")
-	public String patientInfoEditDoctor(@RequestParam int seqAnimal, Model model) {
+	public String patientInfoEditDoctor(@RequestParam int seqAnimal, 
+										Model model) {
 		log.info("환자정보에서 받은 seqAnimal 값 : {}", seqAnimal);
 		DocPatientInfoVO docPatientInfoVO = doctorPageService.selectOnePatientInfoVO(seqAnimal);
 		log.info("수정 화면에 보내줄 환자 개인정보 : {}", docPatientInfoVO);
@@ -121,7 +125,8 @@ public class DoctorPageController {
 	}
 
 	@GetMapping("/diagnosisAdd")
-	public String diagnosisAdd(@RequestParam int seqAnimal, Model model) {
+	public String diagnosisAdd(@RequestParam int seqAnimal, 
+							   Model model) {
 		log.info("받은 seqAnimal 값 : {}", seqAnimal);
 		DocPatientInfoVO docPatientInfoVO = doctorPageService.selectOnePatientInfoVO(seqAnimal);
 		List<MedicineInfoVO> medicineInfoVOs = doctorPageService.selectAllMedicineInfo();
@@ -139,8 +144,10 @@ public class DoctorPageController {
 	}
 
 	@PostMapping("/diagnosisAddOk")
-	public String diagnosisAddOk(@RequestParam Map<String, Object> map, @RequestParam List<Integer> seqMedicineList,
-			@RequestParam List<String> medicineNameList, @RequestParam List<String> medicationGuideList) {
+	public String diagnosisAddOk(@RequestParam Map<String, Object> map, 
+								 @RequestParam List<Integer> seqMedicineList,
+								 @RequestParam List<String> medicineNameList, 
+								 @RequestParam List<String> medicationGuideList) {
 		// #########################################################
 		// ## seqDoctor는 로그인 정보 Session에서 받아와야 합니다.##
 		// #########################################################
@@ -154,11 +161,27 @@ public class DoctorPageController {
 		return "redirect:patientInfo?seqAnimal=" + map.get("seqAnimal");
 	}
 
+	// #####################################################################################################
+	// #### 수의사 전용 페이지 -> 진료내역 조회 -> 환자 정보및 이전 진료 내역 -> 진료내용 작성(추천사료 추가) ####
+	// #####################################################################################################
 	@PostMapping("/diagnosisAddFeedOk")
 	@ResponseBody
 	public String insertFeedExcelUpload(@RequestParam Map<String, String> feedJsonMap) {
 		log.info("insertFeedExcelUpload 실행, 화면에서 넘어온 값(컨트롤러) : {} {}", feedJsonMap);
 		doctorPageService.insertFeedExcelUpload(feedJsonMap);
 		return "diagnosisAdd";
+	}
+	
+	@PostMapping("/patientInfoListOk")
+	@ResponseBody
+	public String selectOneAnimalPatientInfoListVO(@RequestParam(defaultValue = "1") int c, 
+												   @RequestParam(defaultValue = "10") int p,
+												   @RequestParam(defaultValue = "10") int b,
+												   String animalName) {
+		// 서비스에 넘길 환자이름, 의사번호 변수에 담기
+		int seqDoctor = 1;
+		log.info("selectOneAnimalPatientInfoListVO 화면에서 넘어온 값 : {} {} {} {} {}", animalName, seqDoctor, c, p, b);
+		PagingVO<DocPatientInfoVO> pagingVO = doctorPageService.selectOneAnimalPatientInfoListVO(seqDoctor, c, p, b, animalName);
+		return "patientInfoList";
 	}
 }
